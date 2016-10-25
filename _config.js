f6788a4eb4a04329e3a00b65f211a8cae732dc23
bloomlj <kollocc@live.com>
2016-10-25 23:46:18 +0800

    fix gitignore

---
+++ _config.js
@@ -0,0 +1,20 @@
+var ids = {
+  github: {
+    clientID: 'b32a25a08154edb53625',
+    clientSecret: '1bd4d085e140497cc78d3a910ce2042e17431315',
+    callbackURL: "http://www.pitopia.cc:3000/auth/github/callback"
+  },
+  linkedin: {
+    clientID: 'get_your_own',
+    clientSecret: 'get_your_own',
+    callbackURL: "http://127.0.0.1:3000/auth/linkedin/callback"
+  },
+  twitter: {
+    consumerKey: 'get_your_own',
+    consumerSecret: 'get_your_own',
+    callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
+  }
+};
+
+
+module.exports = ids;
